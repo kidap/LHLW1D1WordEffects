eac0e801d289af6bcd6809f75ba1a2a2764426a0
Karlo Pagtakhan <kidap@yahoo.com>
2016-03-07 18:14:06 -0500

    First commit

---
+++ LHLW1D1WordEffects/main.m
@@ -0,0 +1,130 @@
+//
+//  main.m
+//  LHLW1D1WordEffects
+//
+//  Created by Karlo Pagtakhan on 03/07/2016.
+//  Copyright © 2016 AccessIT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+    // insert code here...
+    NSLog(@"Hello, World!");
+    
+    //Variable with 255 characters
+    char optionSelected[2];
+    int tmp;
+    int continueplaying = 1;
+    char inputChars[255];
+    char play[2];
+    int lastCharIndex;
+    NSString *lastChar = [[NSString alloc] init];
+    NSString *operationSelected = [[NSString alloc] init];
+    NSString *playAgain = [[NSString alloc] init];
+    
+    while (continueplaying == 1) {
+      
+      //Ask for the operation
+      printf("Select an operation");
+      printf("\n\t(0)Uppercase");
+      printf("\n\t(1)Lowercase");
+      printf("\n\t(2)Numberize");
+      printf("\n\t(3)Canadianize");
+      printf("\n\t(4)Respond");
+      printf("\n\t(5)De-Space-It");
+      printf("\nEnter the number:");
+      
+      
+      //Get the operation from the input
+      fgets(optionSelected, 2, stdin);
+      printf("You selected option %s.\n", optionSelected);
+      
+      operationSelected = [NSString stringWithUTF8String:optionSelected];
+      
+      //Flush 'Enter' key
+      while ((tmp = getchar()) != '\n' && tmp != EOF);
+      
+      //Get the string
+      printf("Enter the string:");
+      fgets(inputChars, 255, stdin);
+      
+      
+      //Convert the string to NSString
+      NSString *inputString = [NSString stringWithUTF8String:inputChars];
+      NSString *resultString = [[NSString alloc] init];
+      
+      if ([operationSelected isEqualToString:@"0"]){
+        resultString = [inputString uppercaseString];
+        NSLog(@"Input was %@", inputString);
+        NSLog(@"The result of the operation is %@", resultString);
+      } else if ([operationSelected isEqualToString:@"1"]){
+        resultString = [inputString lowercaseString];
+        NSLog(@"Input was %@", inputString);
+        NSLog(@"The result of the operation is %@", resultString);
+      } else if ([operationSelected isEqualToString:@"2"]){
+        //inputString = []
+        int resultNum = 0;
+        
+        if ([inputString rangeOfCharacterFromSet:[NSCharacterSet characterSetWithCharactersInString:@"0123456789"]].location != NSNotFound){
+          resultNum = inputString.intValue;
+          NSLog(@"Input was %@", inputString);
+          NSLog(@"Result is %d", resultNum);
+        } else {
+          NSLog(@"Input was %@", inputString);
+          NSLog(@"Input is not a number");
+        }
+        
+      } else if ([operationSelected isEqualToString:@"3"]){
+        resultString = [inputString stringByAppendingString:@"eh!"];
+        NSLog(@"Input was %@", inputString);
+        NSLog(@"Result is %@", resultString);
+        
+      } else if ([operationSelected isEqualToString:@"4"]){
+        /*
+         If the user input ends with a question mark, answer "I don't know".
+         If the input ends with an exclaimation point, respond with "Whoa, calm down!".
+         */
+        
+        lastCharIndex = [inputString length] - 2;
+        lastChar = [inputString substringWithRange:NSMakeRange(lastCharIndex, 1)];
+        
+        if ([lastChar isEqualToString:@"!"]){
+          NSLog(@"Whoa, calm down!");
+        } else if ([lastChar isEqualToString:@"?"]){
+          NSLog(@"I don't know");
+        }
+        
+        
+      } else if ([operationSelected isEqualToString:@"5"]){
+        resultString = [NSString stringWithString: inputString];
+        resultString = [inputString stringByReplacingOccurrencesOfString:@" " withString:@"-"];
+        NSLog(@"Input was %@", inputString);
+        NSLog(@"Result is %@", resultString);
+        
+        
+      } else {
+        printf("\nInvalid entered operation.");
+        resultString = nil;      }
+      
+      
+      printf("\n\nDo you want do it again? ('Y' for yes)");
+      //scanf("%c", &play);
+      fgets(play, 2, stdin);
+      
+      playAgain = [NSString stringWithUTF8String:play];
+      
+      if ([playAgain isEqualToString:@"Y"]){
+        continueplaying = 1;
+      } else {
+        continueplaying = 0;
+      }
+      
+      //Flush 'Enter' key
+      while ((tmp = getchar()) != '\n' && tmp != EOF);
+    }
+    
+  }
+  return 0;
+}
