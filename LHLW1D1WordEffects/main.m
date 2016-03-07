64411b5267d0066fa82986fc778dda9702efb9f9
Karlo Pagtakhan <kidap@yahoo.com>
2016-03-07 18:25:17 -0500

    Updated

--- LHLW1D1WordEffects/main.m
+++ LHLW1D1WordEffects/main.m
@@ -9,10 +9,7 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
-  @autoreleasepool {
-    // insert code here...
-    NSLog(@"Hello, World!");
-    
+  @autoreleasepool {    
     //Variable with 255 characters
     char optionSelected[2];
     int tmp;
